044630ceac8470bb9d7f847425933c0c0985f051
Per Friis <per.friis@friisconsult.com>
2024-09-14 08:23:51 +0200

    Initial Commit

---
+++ Instrument Simulator/Instrument_SimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instrument_SimulatorApp.swift
+//  Instrument Simulator
+//
+//  Created by Per Friis on 14/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Instrument_SimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
